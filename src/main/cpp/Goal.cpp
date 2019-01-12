1c731bfe629ffd11affa9275de5d4649acb7e291
Chris Weeks <chrisrweeks@aol.com>
2019-01-12 16:50:13 -0600

    changes to goal.h/cpp

--- src/main/cpp/Goal.cpp
+++ src/main/cpp/Goal.cpp
@@ -4,25 +4,50 @@
 
 ////////////////Implemented Atomic Goals////////////////
 /////GoalDoNothing/////
-void GoalDoNothing::Plan()
-{
-    //TODO: decide what to check when planning
-}
 
-Goal::Goal_Status GoalDoNothing::Process()
+Goal::Goal_Status GoalDoNothing::Process(double deltaTime)
 {
+    //if(deltaTime >= timeOut) return eFailed; //this line will not be excluded in other goals
+
     if(m_status == eActive)
     {
-        //this goal does not do anything
-        return eCompleted;
+        if(deltaTime>=m_timeOut) 
+        {
+            Terminate();
+            return m_status;
+        }
     }
     else
     {
         return eErrorNotActive;
     }
-    return eFailed; //if somehow it gets past your code, fail
+    return m_status;
 }
 
 void GoalDoNothing::Terminate()
 {
+    m_status = eCompleted; //doNothing has nothing to test. Other goals may test whether goal should complete or fail
+}
+
+/////GoalDriveWithTimer/////
+Goal::Goal_Status GoalDriveWithTimer::Process(double deltaTime)
+{
+    if(m_status == eActive)
+    {
+        if(deltaTime >= m_timeOut)
+        {
+            Terminate();
+            return m_status;
+        }
+        else
+        {
+            SetDrive(m_left, m_right, m_activeCollection);
+        }
+    }
+}
+
+void GoalDriveWithTimer::Terminate()
+{
+    StopDrive(m_activeCollection);
+    m_status = eCompleted;
 }
