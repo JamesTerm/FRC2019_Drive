e5d27ed66b29908fe732a92445c0525dcf2fcfe8
Chris Weeks <chrisrweeks@aol.com>
2019-01-12 10:21:27 -0600

    Addistions to goal.h/cpp

--- src/main/cpp/Goal.cpp
+++ src/main/cpp/Goal.cpp
@@ -1,3 +1,24 @@
 //Chris's version of goals. just testing stuff.
 
 #include "Goal.h"
+
+////////////////Implemented Atomic Goals////////////////
+/////GoalDoNothing/////
+void GoalDoNothing::Plan()
+{
+    //TODO: decide what to check when planning
+}
+
+Goal::Goal_Status GoalDoNothing::Process()
+{
+    if(m_status == eActive)
+    {
+        //this goal does not do anything
+        return eCompleted;
+    }
+    else
+    {
+        return eErrorNotActive;
+    }
+    return eFailed; //if somehow it gets past your code, fail
+}
