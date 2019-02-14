4ee0472ae960420f65054de98cad5f5eb3b1b26f
Ian Poll <irobot983@gmail.com>
2019-02-14 17:37:08 -0600

    Global.h file made

---
+++ src/main/cpp/Global.h
@@ -0,0 +1,17 @@
+/****************************** Header ******************************\
+Class Name: Global
+File Name:	Global.h
+Summary: Contains h files to include
+Project:     BroncBotzFRC2019
+Copyright (c) BroncBotz.
+All rights reserved.
+
+Author(s): Ian Poll
+Email: irobot9803@gmail.com
+\********************************************************************/
+
+#pragma once
+
+#include "Util/Log.h"
+
+using namespace Logger;
